f24fa015d910833e5fb3a638bdf2e9aa3705fa27
Kaoutar Afkhar <kaafkhar@e1r12p5.1337.ma>
2024-12-21 00:25:43 +0100

    first push test test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: asebrani <asebrani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 11:20:43 by asebrani          #+#    #+#             */
-/*   Updated: 2024/12/12 05:25:23 by asebrani         ###   ########.fr       */
+/*   Updated: 2024/12/10 15:19:55 by asebrani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,18 @@
 
 int	main(int ac, char **av)
 {
-
-	t_map *map;
+	t_mlx	*mlx;
+	t_map	*map;
 
 	map = NULL;
+	mlx = NULL;
 	if (ac != 2 || !av[1])
 		return (write(2,"Invalid arguments\n", 18), 0);
 	if (!parse_map_name(av[1]))
 		return(write(2, "invalid map name\n", 18), 0);
-	if ( 0 != parse_map(av[1] ,&map))
+	if ( 0 != parse_map(av[1] ,map))
 		return(0);
-	create_new_map(&map);
 	parse_map_grid(map);
-	map ->mp_arrs = replace_spaces_with_one(map->mp_arrs);
-	printer(map ->mp_arrs );
+	map_tracing(map, mlx);
 	return(printf("all good\n"), 1);
 }
